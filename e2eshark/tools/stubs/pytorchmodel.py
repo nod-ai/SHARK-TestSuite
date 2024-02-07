1ec5957f5690a31559f2cdfd4b0dda51a6cfc92c
saienduri <enduri.sai@gmail.com>
2024-02-07 12:21:56 -0800

    remove print

--- e2eshark/tools/stubs/pytorchmodel.py
+++ e2eshark/tools/stubs/pytorchmodel.py
@@ -80,7 +80,6 @@ elif runmode == "direct":
             verbose=False,
         )
     else:
-        print(args.outfileprefix)
         torch_mlir_model = fx.export_and_import(model, test_input, model_name=args.outfileprefix)
     with open(torch_mlir_name, "w+") as f:
         f.write(torch_mlir_model.operation.get_asm())
