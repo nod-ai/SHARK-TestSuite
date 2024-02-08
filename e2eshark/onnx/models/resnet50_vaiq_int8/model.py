7b8a8443aa3fb4006926868314df39919de0ae54
Kumar Deepak <kumar@xilinx.com>
2024-02-08 15:20:13 -0800

    add support for list of inputs for onnx and add ms, add and mlp tests

--- e2eshark/onnx/models/resnet50_vaiq_int8/model.py
+++ e2eshark/onnx/models/resnet50_vaiq_int8/model.py
@@ -1,5 +1,6 @@
 import numpy
 import onnxruntime
+import torch
 
 
 # The generated or checked in onnx file must always be called model.onnx
@@ -8,26 +9,27 @@ import onnxruntime
 # through flow
 
 
-# insert here any onnx API call to generate onnx file if
-# not using a checked in onnx model
-
 # start an onnxrt session
 session = onnxruntime.InferenceSession("model.onnx", None)
 
-# test_input and test_output must be numpy
-# bfloat16 is not supported by onnxruntime and numpy
-# case to/from fp32 to work around at various stages
-# fill the lines that set test_input and onnx_output
-# these two are special names and should not be changed
-test_input = numpy.random.rand(1, 3, 224, 224).astype(numpy.float32)
-print("Input:", test_input)
+# Even if model is quantized, the inputs and outputs are
+# not, so apply float32
+test_input_X = numpy.random.rand(1, 3, 224, 224).astype(numpy.float32)
 
-# Get the name of the input of the model
-input_name = session.get_inputs()[0].name
-# TBD: Use iobinding and ortvalue explicitly as per
-# https://onnxruntime.ai/docs/api/python/api_summary.html
-# call inference session
-test_output = numpy.array(
-    [session.run([], {input_name: test_input})[0]], dtype=numpy.float32
+# gets X in inputs[0] and Y in inputs[1]
+inputs = session.get_inputs()
+# gets Z in outputs[0]
+outputs = session.get_outputs()
+
+# test_input and test_output are list of numpy arrays
+# each index into list is one input or one output in the
+# order it appears in the model
+test_input = [test_input_X]
+
+test_output = session.run(
+    [outputs[0].name],
+    {inputs[0].name: test_input_X},
 )
+
+print("Input:", test_input)
 print("Output:", test_output)
