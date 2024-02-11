23db83bb86c8b0647d0c5f2e94645e7ab21b218a
Kumar Deepak <kumar@xilinx.com>
2024-02-10 18:13:50 -0800

    support todype casting for bf16 and fp16

--- e2eshark/tools/stubs/onnxmodel.py
+++ e2eshark/tools/stubs/onnxmodel.py
@@ -13,7 +13,7 @@ parser.add_argument(
     "-d",
     "--todtype",
     choices=["default", "fp32", "fp16", "bf16"],
-    default="none",
+    default="default",
     help="If not default, casts model and input to given data type if framework supports model.to(dtype) and tensor.to(dtype)",
 )
 parser.add_argument(
