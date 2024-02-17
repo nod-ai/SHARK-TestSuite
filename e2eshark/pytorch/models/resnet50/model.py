c0826351b4ae4f2446b975d6f3088992afad25a4
Kumar Deepak <kumar@xilinx.com>
2024-02-17 09:19:02 -0800

    Add a dictionary called E2ESHARK_CHECK to contain variables that get checked during run

--- e2eshark/pytorch/models/resnet50/model.py
+++ e2eshark/pytorch/models/resnet50/model.py
@@ -4,19 +4,31 @@ import torch.nn as nn
 import torch_mlir
 from torchvision.models import resnet50, ResNet50_Weights
 
+# These are pieckled and saved and used by tools/stubs python and run.pl.
+# If adding new fields, make sure the field has default value and have updated
+# tools/stubs and run.pl to handle the new fields
+E2ESHARK_CHECK = {
+    # this is input applied to the model
+    "input": None,
+    # this is output gotten from the model
+    "output": None,
+    # Controls how to import a graph from PyTorch into MLIR, options are: compile or fximport
+    "torchmlirimport": "fximport",
+    # By default, the input.to(dtype) is called, set it to False to not do so
+    "inputtodtype": True,
+    # Apply listed function (tools/stub and run.pl must be able to find definition)
+    # on output from target in sequence to post process output and compare the final
+    # output,
+    # Exmaple: "postprocess": [torch.nn.functional.softmax, torch.topk]
+    "postprocess": None,
+}
+
 test_modelname = "resnet50"
 weights = ResNet50_Weights.DEFAULT
 model = resnet50(weights=weights)
 model.eval()
 
-test_input = test_input = torch.randn(1, 3, 224, 224)
-# Flag to prevent casting of input to a different dtype
-keep_input_dtype = False
-test_output = model(test_input)
-print("Input:", test_input)
-print("Output:", test_output)
-# Do not enforce any particular strategy for getting torch MLIR
-# By default set it to None, set it to
-# 'compile' : to force using torch_mllir.compile
-# 'fximport' : to force using PyTorch 2.0 Fx Import
-test_torchmlircompile = None
+E2ESHARK_CHECK["input"] = E2ESHARK_CHECK["input"] = torch.randn(1, 3, 224, 224)
+E2ESHARK_CHECK["output"] = model(E2ESHARK_CHECK["input"])
+print("Input:", E2ESHARK_CHECK["input"])
+print("Output:", E2ESHARK_CHECK["output"])
