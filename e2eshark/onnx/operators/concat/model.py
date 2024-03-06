c4170d45108c2a2fd0be1a40f2844125f191575c
jinchen <49575973+jinchen62@users.noreply.github.com>
2024-03-06 15:32:02 -0800

    Add onnx concat op test (#94)

---
+++ e2eshark/onnx/operators/concat/model.py
@@ -0,0 +1,80 @@
+# Copyright 2024 Advanced Micro Devices
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# run.py creates runmodel.py by concatenating this file model.py
+# and tools/stubs/onnxmodel.py
+# Description: testing Concat
+# See https://onnx.ai/onnx/intro/python.html for intro on creating
+# onnx model using python onnx API
+import numpy, torch, sys
+import onnxruntime
+from onnx import TensorProto
+from onnx.helper import (
+    make_model,
+    make_node,
+    make_graph,
+    make_tensor_value_info,
+)
+
+# import from e2eshark/tools to allow running in current dir, for run through
+# run.pl, commutils is symbolically linked to allow any rundir to work
+sys.path.insert(0, "../../../tools/stubs")
+from commonutils import E2ESHARK_CHECK_DEF
+
+# Create an instance of it for this test
+E2ESHARK_CHECK = dict(E2ESHARK_CHECK_DEF)
+
+# Create an input (ValueInfoProto)
+X = make_tensor_value_info("X", TensorProto.FLOAT, [1, 4, 5])
+Y = make_tensor_value_info("Y", TensorProto.FLOAT, [2, 4, 5])
+
+# Create an output
+Z = make_tensor_value_info("Z", TensorProto.FLOAT, [3, 4, 5])
+
+# Create a node (NodeProto)
+concat_node = make_node(
+    op_type="Concat",
+    inputs=["X", "Y"],
+    outputs=["Z"],
+    axis=0,
+)
+
+# Create the graph (GraphProto)
+graph = make_graph(
+    nodes=[concat_node],
+    name="concat_graph",
+    inputs=[X, Y],
+    outputs=[Z],
+)
+
+# Create the model (ModelProto)
+onnx_model = make_model(graph)
+onnx_model.opset_import[0].version = 19
+
+# Save the model
+with open("model.onnx", "wb") as f:
+    f.write(onnx_model.SerializeToString())
+
+session = onnxruntime.InferenceSession("model.onnx", None)
+model_input_X = numpy.random.randn(1, 4, 5).astype(numpy.float32)
+model_input_Y = numpy.random.randn(2, 4, 5).astype(numpy.float32)
+inputs = session.get_inputs()
+outputs = session.get_outputs()
+
+model_output = session.run(
+    [outputs[0].name],
+    {inputs[0].name: model_input_X, inputs[1].name: model_input_Y},
+)
+
+# Moving to torch to handle bfloat16 as numpy does not support bfloat16
+E2ESHARK_CHECK["input"] = [
+    torch.from_numpy(model_input_X),
+    torch.from_numpy(model_input_Y),
+]
+E2ESHARK_CHECK["output"] = [torch.from_numpy(arr) for arr in model_output]
+
+print("Input:", E2ESHARK_CHECK["input"])
+print("Output:", E2ESHARK_CHECK["output"])
