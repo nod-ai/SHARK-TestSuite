f132da31c656d53ee89932ac8744549db491061c
Scott Todd <scott.todd0@gmail.com>
2024-03-13 10:28:58 -0700

    Use azure-storage-blob to download Azure files.

--- iree_tests/download_remote_files.py
+++ iree_tests/download_remote_files.py
@@ -4,31 +4,40 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from azure.storage.blob import BlobServiceClient, ContainerClient
 from pathlib import Path
 import pyjson5
-import requests
 
 THIS_DIR = Path(__file__).parent
 
 
-# TODO(scotttodd): checksum / stamp to avoid re-downloading
-# TODO(scotttodd): fast path for known hosts?
-#     https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-download-python
-def download_file(uri: str, dest: Path) -> Path:
-    req = requests.get(uri, stream=True, timeout=60)
-    if not req.ok:
-        raise RuntimeError(f"Failed to fetch {uri}: {req.status_code} - {req.text}")
-    with dest.open("wb") as dest_file:
-        chunk_size = 512 * 1024 * 1024
-        block = 0
-        for data in req.iter_content(chunk_size=chunk_size):
-            dest_file.write(data)
-            block += chunk_size
-            print(f"    {block}")
-    return dest
+# TODO(scotttodd): multithread? async?
+# TODO(scotttodd): skip download if already exists? check some metadata
+def download_azure_remote_files(
+    test_dir: Path, container_client: ContainerClient, remote_file_group: dict
+):
+    base_blob_name = remote_file_group["azure_base_blob_name"]
+
+    for remote_file in remote_file_group["files"]:
+        print(f"  Downloading {remote_file} to {test_dir.relative_to(THIS_DIR)}")
+        blob_name = base_blob_name + remote_file
+        dest = test_dir / remote_file
+
+        with open(dest, mode="wb") as local_blob:
+            download_stream = container_client.download_blob(blob_name)
+            local_blob.write(download_stream.readall())
 
 
-# TODO(scotttodd): multithread, see onnx/import_tests.py
+def download_for_test_case(test_dir: Path, test_case_json: dict):
+    for remote_file_group in test_case_json["remote_file_groups"]:
+        account_url = remote_file_group["azure_account_url"]
+        container_name = remote_file_group["azure_container_name"]
+
+        with BlobServiceClient(account_url) as blob_client:
+            container_client = blob_client.get_container_client(container_name)
+            download_azure_remote_files(test_dir, container_client, remote_file_group)
+            return
+
 
 if __name__ == "__main__":
     for test_cases_path in Path(THIS_DIR).rglob("test_cases.json"):
@@ -38,12 +47,4 @@ if __name__ == "__main__":
         with open(test_cases_path) as f:
             test_cases_json = pyjson5.load(f)
             for test_case_json in test_cases_json["test_cases"]:
-                for remote_file_group in test_case_json["remote_files"]:
-                    base_url = remote_file_group["base_url"]
-                    for remote_file in remote_file_group["files"]:
-                        print(
-                            f"  Downloading {remote_file} to {test_dir.relative_to(THIS_DIR)}"
-                        )
-                        uri = base_url + remote_file
-                        dest = test_dir / remote_file
-                        download_file(uri, dest)
+                download_for_test_case(test_dir, test_case_json)
