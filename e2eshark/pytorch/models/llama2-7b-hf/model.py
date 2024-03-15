79240fd56328ed6a2c777f82457f39cbd97f1916
saienduri <77521230+saienduri@users.noreply.github.com>
2024-03-15 13:26:44 -0700

    add output selection and postprocessing for more accurate validation (#108)
    This commit addresses mismatches we were seeing at inference stage.
    There were two main problems. We were comparing every output generated
    by the model, when we should just be looking at the important outputs
    (ones that influence our prediction). Based on whether there is a head
    attached to the bare model, this is either the logits or last hidden
    state. So added support so we can compare a specific output (modifying
    the e2eshark dict and run.py). Next problem was with the logits. We
    should be comparing the normalized logits for a model output. This
    required adding a SoftMax postprocessing pass (normalizing the `(B, S,
    V)` logit output over the `V` dimension so we have a probability
    distribution of all possible tokens at all sequence positions). I didn't
    want to add topk to the postprocessing because that would hardcode us to
    specific model configuration/prompts. It might work for a certain
    configuration, but the inaccuracies in prediction we ignored can lead to
    inconsistencies with other configurations. Lastly, I added support so
    that user is notified with a message when there are NaN values in the
    output tensors, so the user is aware of the exact numeric issues.

--- e2eshark/pytorch/models/llama2-7b-hf/model.py
+++ e2eshark/pytorch/models/llama2-7b-hf/model.py
@@ -28,6 +28,7 @@ prompt = "What is nature of our existence?"
 encoding = tokenizer(prompt, return_tensors="pt")
 E2ESHARK_CHECK["input"] = encoding["input_ids"].cpu()
 E2ESHARK_CHECK["output"] = model(E2ESHARK_CHECK["input"])
+E2ESHARK_CHECK["output_for_validation"] = [E2ESHARK_CHECK["output"][0]]
 model_response = model.generate(
     E2ESHARK_CHECK["input"],
     do_sample=True,
@@ -43,3 +44,13 @@ print("Output:", E2ESHARK_CHECK["output"])
 # For geneartive AI models, input is int and should be kept that way for
 # casted models as well
 E2ESHARK_CHECK["inputtodtype"] = False
+
+# Post process output to do:
+# torch.nn.functional.softmax(output, -1)
+# The output logits is the shape of (B, S, V).
+# (batch size, sequence length, unormalized scores for each possible token in vocabulary)
+# This way we create a probability distribution for each possible token (vocabulary)
+# for each position in the sequence by doing softmax over the last dimension.
+E2ESHARK_CHECK["postprocess"] = [
+    (torch.nn.functional.softmax, [-1], False, 0),
+]
