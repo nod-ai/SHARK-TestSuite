9ea7c72f5f4f0399199b53a9b25886ab73a0dc5c
saienduri <saimanas.enduri@amd.com>
2024-04-04 15:21:57 -0700

    try variation

--- iree_tests/benchmarks/benchmark_sdxl_cpu.sh
+++ iree_tests/benchmarks/benchmark_sdxl_cpu.sh
@@ -16,6 +16,6 @@ prompt_encoder_dir="$repo_root/pytorch/models/sdxl-prompt-encoder-tank"
 
 echo "Running sdxl benchmark"
 
-iree-benchmark-module --device=local-task --module="$prompt_encoder_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb" --parameters=model="$prompt_encoder_dir/real_weights.irpa" --module="$scheduled_unet_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb" --parameters=model="$scheduled_unet_dir/real_weights.irpa" --module="$vae_decode_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb" --parameters=model="$vae_decode_dir/real_weights.irpa" --module="$this_dir/sdxl_full_pipeline_fp16_.vmfb" --function=tokens_to_image --input=1x4x128x128xf16 --input=1xf16 --input=1x64xi64 --input=1x64xi64 --input=1x64xi64 --input=1x64xi64
+iree-benchmark-module --device=local-task --module=$prompt_encoder_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb --parameters=model="$prompt_encoder_dir/real_weights.irpa" --module="$scheduled_unet_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb" --parameters=model="$scheduled_unet_dir/real_weights.irpa" --module="$vae_decode_dir/model_sdxl_cpu_llvm_task_real_weights.vmfb" --parameters=model="$vae_decode_dir/real_weights.irpa" --module="$this_dir/sdxl_full_pipeline_fp16_.vmfb" --function=tokens_to_image --input=1x4x128x128xf16 --input=1xf16 --input=1x64xi64 --input=1x64xi64 --input=1x64xi64 --input=1x64xi64
 
 echo "Succesfully finished sdxl pipeline benchmark"
