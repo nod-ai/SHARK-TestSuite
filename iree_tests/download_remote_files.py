cd5c848970150c1d01eeb5b29678687b2bdda70e
Scott Todd <scott.todd0@gmail.com>
2024-04-16 09:22:15 -0700

    Switch remote file downloading to use 'logging' module.

--- iree_tests/download_remote_files.py
+++ iree_tests/download_remote_files.py
@@ -8,11 +8,13 @@ from azure.storage.blob import BlobClient, BlobProperties
 from pathlib import Path
 import argparse
 import hashlib
+import logging
 import mmap
 import pyjson5
 import re
 
 THIS_DIR = Path(__file__).parent
+logger = logging.getLogger(__name__)
 
 
 def human_readable_size(size, decimal_places=2):
@@ -23,24 +25,27 @@ def human_readable_size(size, decimal_places=2):
     return f"{size:.{decimal_places}f} {unit}"
 
 
-def check_azure_remote_file_matching_local(
-    blob_properties: BlobProperties, local_file_path: Path
-):
-    if not local_file_path.exists() or local_file_path.stat().st_size == 0:
-        return False
-
-    # Ask Azure for the md5 hash of the remote file.
+def get_remote_md5(remote_file: str, blob_properties: BlobProperties):
     content_settings = blob_properties.get("content_settings")
     if not content_settings:
-        return False
+        return None
     remote_md5 = content_settings.get("content_md5")
+    if not remote_md5:
+        logger.warning(
+            f"  Remote file '{remote_file}' on Azure is missing the "
+            "'content_md5' property, can't check if local matches remote"
+        )
+    return remote_md5
 
-    # Compute the md5 hash of the local file.
+
+def get_local_md5(local_file_path: Path):
+    if not local_file_path.exists() or local_file_path.stat().st_size == 0:
+        return None
+
     with open(local_file_path) as file, mmap.mmap(
         file.fileno(), 0, access=mmap.ACCESS_READ
     ) as file:
-        local_md5 = hashlib.md5(file).digest()
-        return local_md5 == remote_md5
+        return hashlib.md5(file).digest()
 
 
 def download_azure_remote_file(test_dir: Path, remote_file: str):
@@ -73,14 +78,30 @@ def download_azure_remote_file(test_dir: Path, remote_file: str):
         max_single_get_size=1024 * 1024 * 32,  # 32 MiB
     ) as blob_client:
         blob_properties = blob_client.get_blob_properties()
+        blob_size_str = human_readable_size(blob_properties.size)
+        remote_md5 = get_remote_md5(remote_file, blob_properties)
 
         local_file_path = test_dir / remote_file_name
-        if check_azure_remote_file_matching_local(blob_properties, local_file_path):
-            print(f"  Skipping '{remote_file_name}' download (local MD5 hash matches)")
+        local_md5 = get_local_md5(local_file_path)
+
+        if remote_md5 and remote_md5 == local_md5:
+            logger.info(
+                f"  Skipping '{remote_file_name}' download ({blob_size_str}) "
+                "- local MD5 hash matches"
+            )
             return
 
-        size_str = human_readable_size(blob_properties.size)
-        print(f"  Downloading '{remote_file_name}' ({size_str}) to '{relative_dir}'")
+        if not local_md5:
+            logger.info(
+                f"  Downloading '{remote_file_name}' ({blob_size_str}) "
+                f"to '{relative_dir}'"
+            )
+        else:
+            logger.info(
+                f"  Downloading '{remote_file_name}' ({blob_size_str}) "
+                f"to '{relative_dir}' (local MD5 does not match)"
+            )
+
         with open(local_file_path, mode="wb") as local_blob:
             download_stream = blob_client.download_blob(max_concurrency=4)
             local_blob.write(download_stream.readall())
@@ -121,6 +142,12 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # Adjust logging levels.
+    logging.basicConfig(level=logging.INFO)
+    for log_name, log_obj in logging.Logger.manager.loggerDict.items():
+        if log_name.startswith("azure"):
+            logging.getLogger(log_name).setLevel(logging.WARNING)
+
     # TODO(scotttodd): build list of files _then_ download
     # TODO(scotttodd): report size needed for requested files and size available on disk
 
@@ -130,7 +157,7 @@ if __name__ == "__main__":
             if test_cases_json.get("file_format", "") != "test_cases_v0":
                 continue
 
-            print(f"Processing {test_cases_path.relative_to(THIS_DIR)}")
+            logger.info(f"Processing {test_cases_path.relative_to(THIS_DIR)}")
 
             test_dir = test_cases_path.parent
             for test_case_json in test_cases_json["test_cases"]:
