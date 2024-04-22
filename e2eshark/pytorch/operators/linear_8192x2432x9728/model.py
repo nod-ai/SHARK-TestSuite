760fd7577f180d332604f7d2ea2c1d49ceca6995
kumardeepakamd <123522031+kumardeepakamd@users.noreply.github.com>
2024-04-22 11:10:53 -0700

    Add gemm and mlp shapes for sd (#193)
    Co-authored-by: Kumar Deepak <kumar@xilinx.com>

---
+++ e2eshark/pytorch/operators/linear_8192x2432x9728/model.py
@@ -0,0 +1,34 @@
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import torch, sys
+import torch.nn as nn
+
+# import from e2eshark/tools to allow running in current dir, for run through
+# run.pl, commutils is symbolically linked to allow any rundir to work
+sys.path.insert(0, "../../../tools/stubs")
+from commonutils import E2ESHARK_CHECK_DEF
+
+# Create an instance of it for this test
+E2ESHARK_CHECK = dict(E2ESHARK_CHECK_DEF)
+
+
+class mlp_8192x2432x9728(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(2432, 9728),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+model = mlp_8192x2432x9728()
+E2ESHARK_CHECK["input"] = torch.randn(8192, 2432)
+E2ESHARK_CHECK["output"] = model(E2ESHARK_CHECK["input"]).detach()
+print("Input:", E2ESHARK_CHECK["input"])
+print("Output:", E2ESHARK_CHECK["output"])
