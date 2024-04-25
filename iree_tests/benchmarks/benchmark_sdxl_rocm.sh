8862f612467e90dbdd90de60f83e4567b62af89a
saienduri <77521230+saienduri@users.noreply.github.com>
2024-04-25 13:28:17 -0700

    SDXL ROCm Testing (#194)
    This PR adds ROCm Testing support for all the sdxl models. Main things
    that had to be added was support to add backend + model specific compile
    and runtime flags.

--- iree_tests/benchmarks/benchmark_sdxl_rocm.sh
+++ iree_tests/benchmarks/benchmark_sdxl_rocm.sh
@@ -17,22 +17,21 @@ PROMPT_ENCODER_DIR="${IREE_ROOT?}/pytorch/models/sdxl-prompt-encoder-tank"
 echo "Echo compiling full sdxl pipeline"
 
 iree-compile "${THIS_DIR?}/sdxl_pipeline_bench_f16.mlir" \
-  -iree-hal-target-backends=llvm-cpu \
-  --iree-llvmcpu-target-cpu-features=host \
-  --iree-llvmcpu-distribution-size=32 \
-  -o "${THIS_DIR?}/sdxl_full_pipeline_fp16_cpu.vmfb"
+  --iree-hal-target-backends=rocm \
+  --iree-rocm-target-chip=gfx90a \
+  -o "${THIS_DIR?}/sdxl_full_pipeline_fp16_rocm.vmfb"
 
 echo "Running sdxl benchmark"
 
 iree-benchmark-module \
-  --device=local-task \
-  --module="${PROMPT_ENCODER_DIR?}/model_cpu_llvm_task_real_weights.vmfb" \
+  --device=hip \
+  --module="${PROMPT_ENCODER_DIR?}/model_gpu_rocm_real_weights.vmfb" \
   --parameters=model="${PROMPT_ENCODER_DIR?}/real_weights.irpa" \
-  --module="${SCHEDULED_UNET_DIR?}/model_cpu_llvm_task_real_weights.vmfb" \
+  --module="${SCHEDULED_UNET_DIR?}/model_gpu_rocm_real_weights.vmfb" \
   --parameters=model="${SCHEDULED_UNET_DIR?}/real_weights.irpa" \
-  --module="${VAE_DECODE_DIR?}/model_cpu_llvm_task_real_weights.vmfb" \
+  --module="${VAE_DECODE_DIR?}/model_gpu_rocm_real_weights.vmfb" \
   --parameters=model="${VAE_DECODE_DIR?}/real_weights.irpa" \
-  --module="${THIS_DIR?}/sdxl_full_pipeline_fp16_cpu.vmfb" \
+  --module="${THIS_DIR?}/sdxl_full_pipeline_fp16_rocm.vmfb" \
   --function=tokens_to_image \
   --input=1x4x128x128xf16 \
   --input=1xf16 \
