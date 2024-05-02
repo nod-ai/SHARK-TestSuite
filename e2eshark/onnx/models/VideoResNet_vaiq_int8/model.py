ebeb124ff3f1e1ba5b6633b9835c3e05da4c4a43
saienduri <77521230+saienduri@users.noreply.github.com>
2024-05-01 19:23:04 -0700

    Update all vision model inputs and postprocess. (#216)
    This commit changes the inputs from random to actual images for all
    vision models and also adds postprocessing pipeline to look at top k
    classifications for reference comparison. Results in 11 more models
    passing which were previously mismatching.

--- e2eshark/onnx/models/VideoResNet_vaiq_int8/model.py
+++ e2eshark/onnx/models/VideoResNet_vaiq_int8/model.py
@@ -40,10 +40,7 @@ print("Input:", E2ESHARK_CHECK["input"])
 print("Output:", E2ESHARK_CHECK["output"])
 
 # Post process output to do:
-# sort(topk(torch.nn.functional.softmax(output, 0), 2)[1])[0]
-# Top most probability
-# E2ESHARK_CHECK["postprocess"] = [
-#     (torch.nn.functional.softmax, [0], False, 0),
-#     (torch.topk, [2], True, 1),
-#     (torch.sort, [], True, 0),
-# ]
+E2ESHARK_CHECK["postprocess"] = [
+    (torch.nn.functional.softmax, [0], False, 0),
+    (torch.topk, [1], True, 1),
+]
