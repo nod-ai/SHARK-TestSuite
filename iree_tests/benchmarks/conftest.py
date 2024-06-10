dfe02104cb9d30d6f2e239357c8214b43271f8b2
saienduri <saimanas.enduri@amd.com>
2024-06-10 13:01:51 -0700

    update benchmark values and onnx json

--- iree_tests/benchmarks/conftest.py
+++ iree_tests/benchmarks/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--goldentime-rocm-e2e-ms", action="store", default=1660.2, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-unet-ms", action="store", default=441.2, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-clip-ms", action="store", default=18.2, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-vae-ms", action="store", default=286.5, type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-e2e-ms", action="store", default=1657.2, type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-unet-ms", action="store", default=450.2, type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-clip-ms", action="store", default=18.7, type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-vae-ms", action="store", default=288.5, type=float, help="Golden time to test benchmark")
 
 @pytest.fixture
 def goldentime_rocm_e2e(request):
