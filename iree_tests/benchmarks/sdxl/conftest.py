4ee071ff45e26871191e77773865337b7098d40b
saienduri <saimanas.enduri@amd.com>
2024-06-19 13:25:48 -0700

    update defaults

--- iree_tests/benchmarks/sdxl/conftest.py
+++ iree_tests/benchmarks/sdxl/conftest.py
@@ -1,17 +1,17 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--goldentime-rocm-e2e-ms", action="store", default=1661.5, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-unet-ms", action="store", default=450.5, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-clip-ms", action="store", default=19, type=float, help="Golden time to test benchmark")
-    parser.addoption("--goldentime-rocm-vae-ms", action="store", default=288.5, type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-e2e-ms", action="store", type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-unet-ms", action="store", type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-clip-ms", action="store", type=float, help="Golden time to test benchmark")
+    parser.addoption("--goldentime-rocm-vae-ms", action="store", type=float, help="Golden time to test benchmark")
     parser.addoption("--goldendispatch-rocm-unet", action="store", default=1718, type=int, help="Golden dispatch count to test benchmark")
     parser.addoption("--goldendispatch-rocm-clip", action="store", default=1571, type=int, help="Golden dispatch count to test benchmark")
     parser.addoption("--goldendispatch-rocm-vae", action="store", default=250, type=int, help="Golden dispatch count to test benchmark")
     parser.addoption("--goldensize-rocm-unet-bytes", action="store", default=2088217, type=int, help="Golden vmfb size to test benchmark")
     parser.addoption("--goldensize-rocm-clip-bytes", action="store", default=785493, type=int, help="Golden vmfb size to test benchmark")
     parser.addoption("--goldensize-rocm-vae-bytes", action="store", default=762067, type=int, help="Golden vmfb size to test benchmark")
-    parser.addoption("--gpu-number", action="store", default=6, type=int, help="IREE GPU device number to test on")
+    parser.addoption("--gpu-number", action="store", default=0, type=int, help="IREE GPU device number to test on")
     parser.addoption("--rocm-chip", action="store", default="gfx90a", type=str, help="ROCm target chip configuration of GPU")
 
 @pytest.fixture
