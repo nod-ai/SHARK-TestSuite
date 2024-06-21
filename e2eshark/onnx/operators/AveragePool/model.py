d164f8f335d8fbabcf14c55b8accc8d3342a9164
AmosLewis <Amos_Lewis@foxmail.com>
2024-06-21 00:50:23 +0000

    Add dilation attributes examples

--- e2eshark/onnx/operators/AveragePool/model.py
+++ e2eshark/onnx/operators/AveragePool/model.py
@@ -24,22 +24,24 @@ from commonutils import E2ESHARK_CHECK_DEF
 E2ESHARK_CHECK = dict(E2ESHARK_CHECK_DEF)
 
 # Create an input (ValueInfoProto)
-X = make_tensor_value_info("X", TensorProto.FLOAT, [32, 384, 25, 25])
+# X = make_tensor_value_info("X", TensorProto.FLOAT, [32, 384, 25, 25])
+X = make_tensor_value_info("X", TensorProto.FLOAT, [1,1,4,4])
 
 # Create an output
-Z = make_tensor_value_info("Z", TensorProto.FLOAT, [32, 384, 25, 25])
+# Z = make_tensor_value_info("Z", TensorProto.FLOAT, [32, 384, 25, 25])
+Z = make_tensor_value_info("Z", TensorProto.FLOAT, [1,1,2,2])
 
 # Create a node (NodeProto)
 AveragePoolNode = make_node(
     "AveragePool", ["X"], ["Z"], "AveragePoolNode"  # node name  # inputs  # outputs
 )
 # Create attributes for AveragePoolNode
-AveragePoolNode.attribute.append(make_attribute("ceil_mode", 0))
+AveragePoolNode.attribute.append(make_attribute("ceil_mode", 1))
 AveragePoolNode.attribute.append(make_attribute("count_include_pad", 0))
-AveragePoolNode.attribute.append(make_attribute("pads", [1,1,1,1]))
-AveragePoolNode.attribute.append(make_attribute("kernel_shape", [3, 3]))
+# AveragePoolNode.attribute.append(make_attribute("pads", [1,1,1,1]))
+AveragePoolNode.attribute.append(make_attribute("kernel_shape", [2, 2]))
 AveragePoolNode.attribute.append(make_attribute("strides", [1,1]))
-AveragePoolNode.attribute.append(make_attribute("dilations", [1,1]))
+AveragePoolNode.attribute.append(make_attribute("dilations", [2,2]))
 
 # Create the graph (GraphProto)
 graph = make_graph(
@@ -59,7 +61,8 @@ with open("model.onnx", "wb") as f:
 
 
 session = onnxruntime.InferenceSession("model.onnx", None)
-model_input_X = numpy.random.randn(32, 384, 25, 25).astype(numpy.float32)
+# model_input_X = numpy.random.randn(32, 384, 25, 25).astype(numpy.float32)
+model_input_X = numpy.random.randn(1,1,4,4).astype(numpy.float32)
 # gets X in inputs[0]
 inputs = session.get_inputs()
 # gets Z in outputs[0]
