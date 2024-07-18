b4a06d4e6eb1474f5683e2ebd1e8c5076d9998d9
AmosLewis <Amos_Lewis@foxmail.com>
2024-07-18 11:37:57 -0700

    Delete postprocess for all model.py

--- e2eshark/onnx/models/EfficientNet_b5_vaiq/model.py
+++ e2eshark/onnx/models/EfficientNet_b5_vaiq/model.py
@@ -41,9 +41,3 @@ E2ESHARK_CHECK["output"] = [torch.from_numpy(arr) for arr in model_output]
 
 print("Input:", E2ESHARK_CHECK["input"])
 print("Output:", E2ESHARK_CHECK["output"])
-
-# Post process output to do:
-E2ESHARK_CHECK["postprocess"] = [
-    (torch.nn.functional.softmax, [0], False, 0),
-    (torch.topk, [1], True, 1),
-]
