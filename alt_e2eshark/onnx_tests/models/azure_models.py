0df9f1032d3c6eb65e61e2fa81ae50f4fae72897
zjgarvey <zjgarvey@gmail.com>
2024-10-09 10:36:42 -0500

    fix multiple import related issues

--- alt_e2eshark/onnx_tests/models/azure_models.py
+++ alt_e2eshark/onnx_tests/models/azure_models.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from ..helper_classes import AzureDownloadableModel
 from e2e_testing.registry import register_test
 from e2e_testing.storage import load_test_txt_file
+import onnx
 import onnxruntime
 
 this_file = Path(__file__)
@@ -24,6 +25,19 @@ custom_registry = [
     "opt-125M-awq",
     "opt-125m-gptq",
     "DeepLabV3_resnet50_vaiq_int8",
+]
+
+no_opset_update = [
+    "dm_nfnet_f2.dm_in1k",
+    "dm_nfnet_f3.dm_in1k",
+    "dm_nfnet_f4.dm_in1k",
+    "dm_nfnet_f5.dm_in1k",
+    "dm_nfnet_f6.dm_in1k",
+    "vit_base_r50_s16_384.orig_in21k_ft_in1k",
+    "vit_small_r26_s32_224.augreg_in21k_ft_in1k",
+    "vit_small_r26_s32_384.augreg_in21k_ft_in1k",
+    "vit_tiny_r_s16_p8_224.augreg_in21k_ft_in1k",
+    "vit_tiny_r_s16_p8_384.augreg_in21k_ft_in1k",
 ]
 
 # if the model has significant shape issues, consider applying basic optimizations before import by adding to this list:
@@ -87,7 +101,14 @@ basic_opt = [
     "xcit_tiny_24_p8_384_dist",
 ]
 
+remove_metadata_props = [
+    "resnetv2_50x1_bit.goog_in21k_ft_in1k_vaiq",
+]
+
 custom_registry += basic_opt
+custom_registry += no_opset_update
+custom_registry += remove_metadata_props
+
 # for simple models without dim params or additional customization, we should be able to register them directly with AzureDownloadableModel
 # TODO: many of the models in the text files loaded from above will likely need to be registered with an alternative test info class.
 for t in set(model_names).difference(custom_registry):
@@ -113,3 +134,38 @@ class AzureWithOpt(AzureDownloadableModel):
 
 for t in basic_opt:
     register_test(AzureWithOpt, t)
+
+
+class AzureWithOptAndNoOpsetVersion(AzureWithOpt):
+    def __init__(self, name: str, onnx_model_path: str):
+        super().__init__(name, onnx_model_path)
+        self.opset_version = None
+
+
+for t in no_opset_update:
+    register_test(AzureWithOptAndNoOpsetVersion, t)
+
+
+class AzureRemoveMetadataProps(AzureDownloadableModel):
+    def construct_model(self):
+        super().construct_model()
+        self.remove_duplicate_metadata_props()
+
+    def remove_duplicate_metadata_props(self):
+        model = onnx.load(self.model)
+        metadata = model.metadata_props
+        # unhashable type, so manually check for duplicates
+        to_remove = []
+        for i, m0 in enumerate(metadata):
+            for j in range(i + 1, len(metadata)):
+                if m0 == metadata[j]:
+                    to_remove.append(i)
+        num_removed = 0
+        for i in to_remove:
+            _ = metadata.pop(i - num_removed)
+        mod_model_path = str(Path(self.model).parent / "model.modified.onnx")
+        onnx.save(model, mod_model_path)
+        self.model = mod_model_path
+
+
+register_test(AzureRemoveMetadataProps, "resnetv2_50x1_bit.goog_in21k_ft_in1k_vaiq")
