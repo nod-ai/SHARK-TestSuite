7e8a54c04b03c0071d52a470ca5070ac44cf14cc
David Tanner <dtanner@amd.com>
2024-10-16 14:22:21 +0000

    Adding tests for autopad = SAME_LOWER and VALID

--- alt_e2eshark/onnx_tests/operators/convtranspose.py
+++ alt_e2eshark/onnx_tests/operators/convtranspose.py
@@ -30,7 +30,7 @@ class ConvTransposePads(BuildAModel):
 
 register_test(ConvTransposePads, "convtranspose_pads_test")
 
-class ConvTransposeAutoPad(BuildAModel):
+class ConvTransposeAutoPadSameUpper(BuildAModel):
     def construct_i_o_value_info(self):
         self.input_vi = [
             make_tensor_value_info("X", TensorProto.FLOAT, [1, 1, 3, 3]),
@@ -50,7 +50,51 @@ class ConvTransposeAutoPad(BuildAModel):
             strides=[2, 2],
         )
 
-register_test(ConvTransposeAutoPad, "convtranspose_autopad_test")
+register_test(ConvTransposeAutoPadSameUpper, "convtranspose_autopad_same_upper_test")
+
+class ConvTransposeAutoPadSameLower(BuildAModel):
+    def construct_i_o_value_info(self):
+        self.input_vi = [
+            make_tensor_value_info("X", TensorProto.FLOAT, [1, 1, 3, 3]),
+            make_tensor_value_info("W", TensorProto.FLOAT, [1, 2, 4, 4]),
+        ]
+        self.output_vi = [
+            make_tensor_value_info("Y", TensorProto.FLOAT, [1, 2, 6, 6]),
+        ]
+
+    def construct_nodes(self):
+        app_node = self.get_app_node()
+        app_node(
+            "ConvTranspose",
+            ["X", "W"],
+            ["Y"],
+            auto_pad="SAME_LOWER",
+            strides=[2, 2],
+        )
+
+register_test(ConvTransposeAutoPadSameLower, "convtranspose_autopad_same_lower_test")
+
+class ConvTransposeAutoPadValid(BuildAModel):
+    def construct_i_o_value_info(self):
+        self.input_vi = [
+            make_tensor_value_info("X", TensorProto.FLOAT, [1, 1, 3, 3]),
+            make_tensor_value_info("W", TensorProto.FLOAT, [1, 2, 4, 4]),
+        ]
+        self.output_vi = [
+            make_tensor_value_info("Y", TensorProto.FLOAT, [1, 2, 8, 8]),
+        ]
+
+    def construct_nodes(self):
+        app_node = self.get_app_node()
+        app_node(
+            "ConvTranspose",
+            ["X", "W"],
+            ["Y"],
+            auto_pad="VALID",
+            strides=[2, 2],
+        )
+
+register_test(ConvTransposeAutoPadValid, "convtranspose_autopad_valid_test")
 
 class ConvTransposeAutoPadOutputShape(BuildAModel):
     def construct_i_o_value_info(self):
