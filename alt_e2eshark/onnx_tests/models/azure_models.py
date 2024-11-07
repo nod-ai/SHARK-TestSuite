5ff774f0910d37fc8ee01c10ca0cdc4b3342e702
zjgarvey <47986913+zjgarvey@users.noreply.github.com>
2024-11-07 10:33:41 -0600

    add seven compile failure models to basic opt list (#389)
    1. Three `jx_nest_*` models will newly pass with basic optimizations. 
    2. Three `twins_svt_*` models will now pass iree inference invocation,
    but fail numerics quite terribly.
    3. The `bat_resnext` model will still fail compile due to a similar
    error as in <https://github.com/iree-org/iree/issues/19058>

--- alt_e2eshark/onnx_tests/models/azure_models.py
+++ alt_e2eshark/onnx_tests/models/azure_models.py
@@ -42,6 +42,13 @@ no_opset_update = [
 
 # if the model has significant shape issues, consider applying basic optimizations before import by adding to this list:
 basic_opt = [
+    "bat_resnext26ts.ch_in1k",
+    "twins_svt_base",
+    "twins_svt_large",
+    "twins_svt_small",
+    "jx_nest_base",
+    "jx_nest_small",
+    "jx_nest_tiny",
     "coat_mini",
     "coat_tiny",
     "mvitv2_base",
