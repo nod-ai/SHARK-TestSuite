06846833367f6e91f06f791da5ee851b29b20700
zjgarvey <zjgarvey@gmail.com>
2024-12-17 17:43:49 +0000

    Add requirements and cleanup backends.py

--- alt_e2eshark/e2e_testing/backends.py
+++ alt_e2eshark/e2e_testing/backends.py
@@ -34,20 +34,26 @@ def flag(arg : str) -> str:
     if arg.startswith("--"):
         return arg
     return f'--{arg}'
+
 class SimpleIREEBackend(BackendBase):
     '''This backend uses iree to compile and run MLIR modules for a specified hal_target_backend'''
     def __init__(self, *, device="local-task", hal_target_backend="llvm-cpu", extra_args : List[str] = None):
         self.device = device
         self.hal_target_backend = hal_target_backend
-        self.extra_args = []
-        if extra_args:
-            for a in extra_args:
-                self.extra_args.append(flag(a))
+        self.extra_args = [] if extra_args is None else [flag(a) for a in extra_args]
+        if hal_target_backend == "rocm":
+            self.extra_args += [
+                f"--iree-hip-target={self.target_chip}",
+            ]
+        if hal_target_backend == "llvm-cpu":
+            self.extra_args += [
+                "--iree-llvmcpu-target-cpu=host",
+            ]
 
     def compile(self, module, *, save_to: str = None, extra_options : CompilerOptions):
-        test_specific_args = extra_options.common_extra_args
+        test_specific_args = list(extra_options.common_extra_args)
         if self.hal_target_backend in extra_options.backend_specific_flags.keys():
-            test_specific_args += extra_options.backend_specific_flags[self.hal_target_backend]
+            test_specific_args += list(extra_options.backend_specific_flags[self.hal_target_backend])
         compile_args = self.extra_args + [flag(arg) for arg in test_specific_args]
         # compile to a vmfb for llvm-cpu
         b = ireec.tools.compile_str(
@@ -85,13 +91,7 @@ class CLIREEBackend(BackendBase):
         self.device = device
         self.hal_target_backend = hal_target_backend
         self.target_chip = target_chip
-        self.extra_args = []
-        if extra_args:
-            for a in extra_args:
-                if a[0:2] == "--":
-                    self.extra_args.append(a)
-                else:
-                    self.extra_args.append("--" + a)
+        self.extra_args = [] if extra_args is None else [flag(a) for a in extra_args]
         if hal_target_backend == "rocm":
             self.extra_args += [
                 f"--iree-hip-target={self.target_chip}",
@@ -102,17 +102,16 @@ class CLIREEBackend(BackendBase):
             ]
     
     def compile(self, module_path: str, *, save_to : str = None, extra_options : CompilerOptions) -> str:
-        test_specific_args = extra_options.common_extra_args
+        test_specific_args = list(extra_options.common_extra_args)
         if self.hal_target_backend in extra_options.backend_specific_flags.keys():
-            test_specific_args += extra_options.backend_specific_flags[self.hal_target_backend]
+            test_specific_args += list(extra_options.backend_specific_flags[self.hal_target_backend])
         compile_args = self.extra_args + [flag(arg) for arg in test_specific_args]
         vmfb_path = os.path.join(save_to, "compiled_model.vmfb")
         arg_string = f"--iree-hal-target-backends={self.hal_target_backend} "
-        for arg in compile_args:
-            arg_string += f'{arg} '
+        arg_string += ' '.join(compile_args)
         detail_log = os.path.join(save_to, "detail", "compilation.detail.log")
         commands_log = os.path.join(save_to, "commands", "compilation.commands.log")
-        script = f"iree-compile {module_path} {arg_string}-o {vmfb_path} 1> {detail_log} 2>&1"
+        script = f"iree-compile {module_path} {arg_string} -o {vmfb_path} 1> {detail_log} 2>&1"
         with open(commands_log, "w") as file:
             file.write(script) 
         # remove old vmfb if it exists
@@ -126,9 +125,9 @@ class CLIREEBackend(BackendBase):
     
     def load(self, vmfb_path: str, *, func_name=None, extra_options : RuntimeOptions):
         """A bit hacky. func returns a script that would dump outputs to terminal output. Modified in config.run method"""
-        test_specific_args = extra_options.common_extra_args
+        test_specific_args = list(extra_options.common_extra_args)
         if self.hal_target_backend in extra_options.backend_specific_flags.keys():
-            test_specific_args += extra_options.backend_specific_flags[self.hal_target_backend]
+            test_specific_args += list(extra_options.backend_specific_flags[self.hal_target_backend])
         run_dir = Path(vmfb_path).parent
         def func(x: TestTensors) -> str:
             script = f"iree-run-module --module='{vmfb_path}' --device={self.device} "
@@ -148,11 +147,10 @@ class OnnxrtIreeEpBackend(BackendBase):
     def __init__(self, *, device="local-task", hal_target_device="llvm-cpu", extra_args : List[str] = None):
         self.device = device
         self.hal_target_device = hal_target_device
-        if extra_args:
-            self.extra_args = [flag(a) for a in extra_args]
-        elif hal_target_device == "hip":
+        self.extra_args = [] if extra_args is None else [flag(a) for a in extra_args]
+        if hal_target_device == "hip":
             # some extra args for Mi250 - some of these may not work for other chips
-            self.extra_args = [
+            self.extra_args += [
                 "--iree-hip-target=gfx90a",
             ]
         self.providers = ["IreeExecutionProvider"]
