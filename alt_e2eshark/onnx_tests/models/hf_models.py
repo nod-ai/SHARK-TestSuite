3ac330411fe9771ac8a1d1ac4c337d86bdc7acd4
Vinayak Dev <104419489+vinayakdsci@users.noreply.github.com>
2024-12-20 13:15:47 +0530

    Fix setup failures in Huggingface model tests (#415)
    Updates HF cache locations to respect CACHE_DIR, and fixes a number of
    setup failures in the HF Model tests.
    
    Also prunes the shard lists to remove some models that Optimum does not
    have proper ONNX exports for.

--- alt_e2eshark/onnx_tests/models/hf_models.py
+++ alt_e2eshark/onnx_tests/models/hf_models.py
@@ -12,7 +12,7 @@ from ..helper_classes import HfDownloadableModel
 from e2e_testing.registry import register_test
 from e2e_testing.storage import TestTensors, load_test_txt_file
 
-from transformers import AutoTokenizer, BartTokenizer, BertTokenizer, RobertaTokenizer
+from transformers import AutoTokenizer, BartTokenizer, BertTokenizer, PhobertTokenizer, RobertaTokenizer, XLMRobertaTokenizer
 from torchvision import transforms
 from PIL import Image
 
@@ -34,6 +34,71 @@ task_list = [
     "image-segmentation",
     "semantic-segmentation",
 ]
+
+# These are NLP model names that have a mismatch between tokenizer
+# outputs and the model inputs, but do not fall under a particular
+# model task. If a huge number of models that can be grouped
+# in a common category fall under this list, a new meta_constructor
+# should be created for them.
+update_tokenizer_input_names = [
+    "hf_paraphrase-multilingual-MiniLM-L12-v2",
+    "hf_all-MiniLM-L6-v2",
+    "hf_jina-embeddings-v2-small-en",
+    "hf_all-MiniLM-L12-v2",
+    "hf_msmarco-MiniLM-L6-cos-v5",
+    "hf_paraphrase-MiniLM-L6-v2",
+    "hf_multi-qa-MiniLM-L6-cos-v",
+    "hf_bge-small-en-v1.5",
+    "hf_llm-embedder",
+    "hf_bert-base-nli-mean-tokens",
+    "hf_LaBSE-en-ru",
+    "hf_bge-large-en-v1.5",
+    "hf_bert-base-turkish-cased-mean-nli-stsb-tr",
+    "hf_mxbai-embed-large-v1",
+    "hf_bge-base-en-v1.5",
+    "hf_phobert-large-finetuned",
+    "hf_bertweet-base-sentiment-analysis",
+    "hf_bertweet-base-emotion-analysis",
+    "hf_phobert-base-finetuned",
+    "hf_bge-large-zh-v1.5",
+    "hf_UAE-Large-V1",
+    "hf_GIST-small-Embedding-v0",
+    "hf_rubert-tiny2",
+    "hf_bge-small-en",
+    "hf_bge-large-en",
+    "hf_GIST-Embedding-v0",
+    "hf_GIST-large-Embedding-v0",
+    "hf_paraphrase-MiniLM-L3-v2",
+    "hf_LaBSE",
+    "hf_opensearch-neural-sparse-encoding-doc-v2-distill",
+    "hf_snowflake-arctic-embed-m",
+    "hf_phobert-base-v2",
+    "hf_phobert-base",
+    "hf_bertweet-base",
+]
+
+def get_tokenizer_from_model_path(model_repo_path: str, cache_dir: str | Path):
+    name = model_repo_path.split('/')[-1]
+    if "deberta" in name.lower():
+        return AutoTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    if "bart" in name.lower():
+        return BartTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    if "xlm" in name.lower() and "roberta" in name.lower():
+        return XLMRobertaTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    if "roberta" in name.lower():
+        return RobertaTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    if "phobert" in name.lower():
+        return PhobertTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    if "bert" in name.lower():
+        return BertTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
+    return AutoTokenizer.from_pretrained(model_repo_path, cache_dir=cache_dir)
+
 
 
 def build_repo_to_model_map():
@@ -92,29 +157,11 @@ class HfModelWithTokenizers(HfDownloadableModel):
         model_dir = str(Path(self.model).parent)
         prompt = ["Deeds will not be less valiant because they are unpraised."]
 
-        # Bert/Roberta/Bart models require special tokenizers and might not work best
-        # with an AutoTokenizer. Therefore, switch between tokenizers depending
-        # on the model architecture. More branches might be needed for other
-        # architectures that benefit from a specific tokenizer.
-        tokenizer = None
-        if "deberta" in self.name.lower():
-            # Avoid conflicts with tokenizers of "bert" models.
-            pass
-        elif "bart" in self.name.lower():
-            tokenizer = BartTokenizer.from_pretrained(self.model_repo_path, cache_dir=self.cache_dir)
-        elif "roberta" in self.name.lower():
-            tokenizer = RobertaTokenizer.from_pretrained(self.model_repo_path, cache_dir=self.cache_dir)
-        elif "bert" in self.name.lower():
-            tokenizer = BertTokenizer.from_pretrained(self.model_repo_path, cache_dir=self.cache_dir)
-        else:
-            # Exit the branch. For these cases we use AutoTokenizer.
-            pass
+        tokenizer = get_tokenizer_from_model_path(self.model_repo_path, self.cache_dir)
+        if self.name in update_tokenizer_input_names:
+            tokenizer.model_input_names = ['input_ids', 'attention_mask']
 
-        # At this point, tokenized should be false, so a check is redundant.
-        tokenizer = AutoTokenizer.from_pretrained(self.model_repo_path, cache_dir=self.cache_dir)
-
-
-        tokens = tokenizer(prompt, return_tensors="pt")
+        tokens = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True)
         inputs = (*list(tokens.values()),)
 
         self.input_name_to_shape_map = {k: v.shape for (k, v) in tokens.items()}
@@ -126,7 +173,7 @@ class HfModelWithTokenizers(HfDownloadableModel):
 class HfModelMultipleChoice(HfDownloadableModel):
     def construct_inputs(self):
         model_dir = str(Path(self.model).parent)
-        tokenizer = AutoTokenizer.from_pretrained(self.model_repo_path, cache_dir=self.cache_dir)
+        tokenizer = get_tokenizer_from_model_path(self.model_repo_path, self.cache_dir)
 
         prompt = "France has a bread law, Le Décret Pain, with strict rules on what is allowed in a traditional baguette."
         candidate1 = "The law does not apply to croissants and brioche."
@@ -134,7 +181,7 @@ class HfModelMultipleChoice(HfDownloadableModel):
 
         # For Deberta/Roberta Models, the ONNX export will have a mismatch in the number of inputs.
         # See https://stackoverflow.com/questions/75948679/deberta-onnx-export-does-not-work-for-token-type-ids.
-        if "deberta" in self.name or "roberta" in self.name:
+        if "deberta" in self.name or "roberta" in self.name or self.name in update_tokenizer_input_names:
             tokenizer.model_input_names = ['input_ids', 'attention_mask']
 
         tokens = tokenizer([[prompt, candidate1], [prompt, candidate2]], return_tensors="pt", padding=True)
@@ -171,6 +218,12 @@ class HfModelWithImageSetup(HfDownloadableModel):
 
         model_dir = str(Path(self.model).parent)
         inputs = setup_test_image()
+
+        # TODO: Figure out a way to remove the hardcoded
+        # input name ('pixel_values') and load from model
+        # config/input list.
+        self.input_name_to_shape_map = {'pixel_values': inputs.shape}
+
         test_tensors = TestTensors((inputs,))
         test_tensors.save_to(model_dir)
         return test_tensors
@@ -187,7 +240,7 @@ for t in model_repo_map.keys():
             | "token-classification"
         ):
             register_test(meta_constructor_tokenizer(t), t)
-        case "image-classification" | "object-detection" | "image-segmentation":
+        case "image-classification" | "object-detection" | "image-segmentation" | "semantic-segmentation":
             register_test(meta_constructor_cv(t), t)
         case "multiple-choice":
             register_test(meta_constructor_multiple_choice(t), t)
